45ba38dcd1e7786fba22c5586a6c85bb91c35cdd
Mandip Tamang <136143099+mandiptmg@users.noreply.github.com>
2023-11-25 19:50:12 +0545

    bg-url change

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -4,7 +4,7 @@ const about = () => {
       id='home'
       className=' before:absolute before:top-0 before:left-0 before:h-screen before:w-full   before:bg-gradient-to-b before:-z-10 before:from-transparent before:to-black'
     >
-      <div className='before:bg-blend-overlay before:bg-black/30 before:absolute before:top-0 before:left-0 before:h-screen before:w-full before:bg-[url(src/assets/top.jpg)] before:bg-scroll before:bg-no-repeat before:bg-cover before:-z-20 w-full min-h-screen grid place-items-center'>
+      <div className='before:bg-blend-overlay before:bg-black/30 before:absolute before:top-0 before:left-0 before:h-screen before:w-full before:bg-[url(https://c4.wallpaperflare.com/wallpaper/97/833/155/mountains-firewatch-green-forest-wallpaper-preview.jpg)] before:bg-scroll before:bg-no-repeat before:bg-cover before:-z-20 w-full min-h-screen grid place-items-center'>
         <div className='text-center z-10 '>
           <h1 className='text-3xl md:text-6xl capitalize text-white '>
             the west mountains
